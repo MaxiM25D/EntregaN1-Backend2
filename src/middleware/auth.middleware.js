d45bf5d942b8da6922922c950e418de34d9fc2ad
Maximiliano Morandi <maximorandi19@gmail.com>
2026-02-15 18:06:27 -0300

    Ajustes finales

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,14 +1,31 @@
-export const authorizeRole = (...roles) => {
-  return (req, res, next) => {
+import jwt from "jsonwebtoken";
+import env from "../config/env.config.js";
+import { User } from "../models/user.model.js";
 
-    if (!req.user) {
-      return res.status(401).json({ message: "No autenticado" });
+export const auth = async (req, res, next) => {
+  try {
+
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      return res.status(401).json({ message: "No token provided" });
     }
 
-    if (!roles.includes(req.user.role)) {
-      return res.status(403).json({ message: "No autorizado" });
+    const token = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(token, env.JWT_SECRET);
+
+    const user = await User.findById(decoded.sub);
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid token" });
     }
+
+    req.user = user;
 
     next();
-  };
+
+  } catch (error) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 };
